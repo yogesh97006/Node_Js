c2dc8302697950b487b9851fde7226601ad27685
Yogesh Saini <yogeshsaini9700@gmail.com>
2023-07-07 23:32:41 +0530

    diff b/w async and sync

---
+++ 04_Sync and Async/index.js
@@ -0,0 +1,13 @@
+const fs=require('fs')
+
+// fs.writeFileSync('data.txt','this is to see the difference between async and sync')
+
+// const info=fs.readFileSync('data.txt','utf-8')
+// console.log(info)
+
+// fs.readFile('data.txt','utf-8',(err,data)=>{
+//     console.log(data)
+// })
+
+
+console.log('lets see when it excutes before or after')
