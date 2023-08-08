9ffb23b23623b2cb2abf4971a38dc2afa95bc806
Yogesh Saini <yogeshsaini9700@gmail.com>
2023-08-08 22:42:30 +0530

    praticing

---
+++ 22_pratice-set/File System/index.js
@@ -0,0 +1,46 @@
+const fs=require('fs')
+
+// fs.writeFileSync('data.txt','My Name is Yogesh I am working on currently backend')
+
+// fs.appendFileSync('data.txt','Also trying to master frontend')
+
+// const a=fs.readFileSync('data.txt','utf-8')
+// console.log(a)
+
+// fs.renameSync("data.txt","personal.txt")
+
+// fs.unlinkSync("personal.txt")
+
+
+// fs.mkdir("data",(err)=>{
+//   console.log(err)
+// })
+
+// fs.writeFile('./data/info.txt','My Name is Yogesh Saini',(err)=>{
+//     console.log(err)
+// })
+
+// fs.appendFile('./data/info.txt',' And i am also working',(err)=>{
+//     console.log(err)
+// })
+
+// fs.readFile('./data/info.txt',(err,data)=>{
+//     if(err){
+//     console.log(err)
+//     }
+//     console.log(data.toString())
+// })
+
+
+// fs.rename('./data/info.txt','./data/personal.txt',(err)=>{
+// console.log(err)
+// })
+
+// fs.unlink('./data/personal.txt',(err)=>{
+// console.log(err)
+// })
+
+
+// fs.rmdir('./data',(err)=>{
+// console.log(err)
+// })
